b80a64ed2aeab02118f6ddf6d202f5d9f73b4a65
Yukon Z <yz768737@gmail.com>
2022-11-29 22:02:27 -0700

    Rearranged project + renamed index.js to app.js
    I have restructured the project. In doing so, index.js was changed to app.js. Also, an .env file was created to store the mongodb connection string to properly connect to the cloud cluster.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const fs = require("fs");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("Database Connected"))
+.catch((error) => console.log(error.message));
 
 app.get("/notes", (req, res) => {
     fs.readFile("./testData/notes.json", "utf-8", (err, data) => {
