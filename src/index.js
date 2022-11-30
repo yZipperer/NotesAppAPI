4b032809554c172f2281ffa831696cef0d4cc5fd
Yukon Z <yz768737@gmail.com>
2022-11-29 21:16:25 -0700

    Added Test Data + Test Route
    There is now a directory for a test data file. Also included is a route for testing getting notes.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const fs = require("fs");
 
 const app = express();
+
+app.get("/notes", (req, res) => {
+    fs.readFile("./testData/notes.json", "utf-8", (err, data) => {
+        (err) ? console.log(err) : res.send(data);
+    });
+});
 
 app.listen(8080, () => {
     console.log("Server Started: Port 8080");
