f507f87d03f05fd0ed8450c725dbcdedad94c6a8
Yukon Z <yz768737@gmail.com>
2022-11-30 21:21:58 -0700

    Delete and Update Methods
    There are now routes to update and delete notes from the database.

--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,17 @@
-const Note = require("./models/notes");
+const Notes = require("./models/notes");
 
 app.get("/notes", (req, res) => {
-    fs.readFile("./testData/notes.json", "utf-8", (err, data) => {
-        (err) ? console.log(err) : res.send(data);
+    const notes = Notes.find({})
+    .then(() => {
+        res.status(200).send(notes);
+    })
+    .catch((err) => {
+        res.status(500).send(err);
     });
 });
 
 app.post("/notes", (req, res) => {
-    const note = new Note(req.body);
+    const note = new Notes(req.body);
 
     note.save()
     .then(() => {
@@ -16,4 +20,35 @@ app.post("/notes", (req, res) => {
     .catch((err) => {
         res.status(500).send(err);
     });
+});
+
+app.patch("/notes/:id", async (req, res) => {
+    try{
+        const note = await Notes.findById(req.params.id);
+        
+        if(!note){
+            return res.status(404).send("Note not found");
+        }
+        
+        note.content = req.body.content;
+
+        await note.save();
+        res.status(200).send(note);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.delete("/notes/:id", async (req, res) => {
+    try{
+        const note = await Notes.findByIdAndDelete(req.params.id);
+        
+        if(!note){
+            return res.status(404).send("Note not found");
+        }
+
+        res.status(200).send("Note has been deleted");
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
